0df831a6c863ecfe597c83acc252ec4a84ad6cd6
Anne Culpepper <anne.culpepper@gmail.com>
2020-04-11 16:26:43 -0500

    created an axios GET call for server side gallery and created a GalleryList component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,31 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
+import GalleryList from "../galleryList/galleryList";
 
 class App extends Component {
+  componentDidMount() {
+    this.getGallery();
+  }
+  //getting the gallery photos from the gallery.data.js, which is imported into the server side router, and pulling them into the app.js
+  getGallery() {
+    axios
+      .get("/gallery")
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => console.warn(err));
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of my life</h1>
         </header>
-        <br/>
+        <br />
+        <GalleryList />
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <img src="images/goat_small.jpg" />
       </div>
     );
   }
---
+++ src/components/galleryList/galleryList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class GalleryList extends Component {
+  render() {
+    return (
+      <div>
+        <p>HELLO</p>
+      </div>
+    );
+  }
+}
+
+export default GalleryList;
